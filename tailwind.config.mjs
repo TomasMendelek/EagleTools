bf887677d9a8a62984144386da0c1336758afbbb
tomasmendelek <tomascmendelek@gmail.com>
2024-06-21 15:05:36 -0300

    initial Commit

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,15 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {},
+		colors: {
+			'backgound': '#010104',
+			'primary': '#c22466',
+			'secundary': '#7d09dc',
+			'secundary-200': '#5A05A0c',
+			'accent': '#d78ecc',
+			'text': '#eae9fc',
+
+		  },
 	},
 	plugins: [],
 }
