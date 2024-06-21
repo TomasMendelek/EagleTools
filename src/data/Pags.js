956d0dab43636f62109b612a4cf4f8901c25ddf4
tomasmendelek <tomascmendelek@gmail.com>
2024-06-21 15:06:10 -0300

    integracion sideBar + Paginas + Cards

---
+++ src/data/Pags.js
@@ -0,0 +1,26 @@
+export const Paginas = [
+    {
+        nombre : "IconiFly",
+        categoria : "icons",
+        imagen : "./og-image.png",
+        url : "https://fonts.google.com/icons"
+    },
+    {
+        nombre : "fonts Google",
+        categoria : "icons",
+        imagen : "./og-image.png",
+        url : "https://fonts.google.com/icons"
+    },
+    {
+        nombre : "simple Icons",
+        categoria : "icons",
+        imagen : "./og-image.png",
+        url : "https://fonts.google.com/icons"
+    },
+    {
+        nombre : "svgRepo",
+        categoria : "icons",
+        imagen : "./og-image.png",
+        url : "https://fonts.google.com/icons"
+    }
+]
