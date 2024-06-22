631e8e620f7119603b0a308d57ec249cef693670
Tomas Corbalan Mendelek <143723168+TomasMendelek@users.noreply.github.com>
2024-06-22 14:47:41 -0300

    debug

--- src/data/Pags.js
+++ src/data/Pags.js
@@ -1,26 +1,40 @@
 export const Paginas = [
     {
         nombre : "IconiFly",
-        categoria : "icons",
+        categoria : "typografy",
         imagen : "./og-image.png",
-        url : "https://fonts.google.com/icons"
+        url : "https://fonts.google.com/icons",
+        iconcategoria : "bx bxs-ghost bx-tada"
+        
     },
     {
         nombre : "fonts Google",
         categoria : "icons",
         imagen : "./fontgoogle.jpg",
-        url : "https://fonts.google.com/icons"
+        url : "https://fonts.google.com/icons",
+        iconcategoria : "bx bx-font-family bx-tada"
+
     },
     {
         nombre : "simple Icons",
         categoria : "icons",
         imagen : "./simpleicons.png",
-        url : "https://fonts.google.com/icons"
+        url : "https://fonts.google.com/icons",
+        iconcategoria : "bx bxs-ghost bx-tada"
+
     },
     {
         nombre : "svgRepo",
         categoria : "icons",
         imagen : "./svgrepo.png",
-        url : "https://fonts.google.com/icons"
+        url : "https://fonts.google.com/icons",
+        iconcategoria : "bx bxs-ghost bx-tada"
+    },
+    {
+        nombre : "BoxIcons",
+        categoria : "icons",
+        imagen : "./svgrepo.png",
+        url : "https://boxicons.com/",
+        iconcategoria : "bx bxs-ghost bx-tada"
     }
 ]
