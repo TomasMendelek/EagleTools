dd947b600d3a67089c44c55273f2b3c05e1dc3ad
Tomas Corbalan Mendelek <143723168+TomasMendelek@users.noreply.github.com>
2024-06-22 18:32:22 -0300

    +img

--- src/data/Pags.js
+++ src/data/Pags.js
@@ -1,15 +1,15 @@
 export const Paginas = [
     {
         nombre : "IconiFly",
-        categoria : "typografy",
-        imagen : "./og-image.png",
+        categoria : "icons",
+        imagen : "./og-image.jpg",
         url : "https://fonts.google.com/icons",
         iconcategoria : "bx bxs-ghost bx-tada"
         
     },
     {
         nombre : "fonts Google",
-        categoria : "icons",
+        categoria : "typografy",
         imagen : "./fontgoogle.jpg",
         url : "https://fonts.google.com/icons",
         iconcategoria : "bx bx-font-family bx-tada"
@@ -33,8 +33,80 @@ export const Paginas = [
     {
         nombre : "BoxIcons",
         categoria : "icons",
-        imagen : "./svgrepo.png",
+        imagen : "./boxicons.png",
         url : "https://boxicons.com/",
         iconcategoria : "bx bxs-ghost bx-tada"
+    },
+    {
+        nombre : "Phosphor Icons",
+        categoria : "icons",
+        imagen : "./phosphor-opengraph.webp",
+        url : "https://phosphoricons.com/",
+        iconcategoria : "bx bxs-ghost bx-tada"
+    },
+    {
+        nombre : "svgl",
+        categoria : "icons",
+        imagen : "./screenshot.webp",
+        url : "https://svgl.app/",
+        iconcategoria : "bx bxs-ghost bx-tada"
+    },
+    {
+        nombre : "BootsTrap Icons",
+        categoria : "icons",
+        imagen : "./bootstrap-icons-social.webp",
+        url : "https://svgl.app/",
+        iconcategoria : "bx bxs-ghost bx-tada"
+    },
+    {
+        nombre : "bg.ibelick",
+        categoria : "Backgrounds",
+        imagen : "https://res.cloudinary.com/cosmocloudinary/image/upload/v1703614372/freesets/backgrounds/byn5pbw6ikqp95yyyske.webp",
+        url : "https://bg.ibelick.com/",
+        iconcategoria : "bx bxs-ghost bx-tada"
+    },
+    {
+        nombre : "UVcanvas",
+        categoria : "Backgrounds",
+        imagen : "https://res.cloudinary.com/cosmocloudinary/image/upload/freesets/backgrounds/uvcanvas",
+        url : "https://res.cloudinary.com/cosmocloudinary/image/upload/freesets/backgrounds/uvcanvas",
+        iconcategoria : "bx bxs-ghost bx-tada"
+    },
+    {
+        nombre : "Pattern Moster",
+        categoria : "Backgrounds",
+        imagen : "https://res.cloudinary.com/cosmocloudinary/image/upload/freesets/backgrounds/pattern_monster",
+        url : "https://pattern.monster/",
+        iconcategoria : "bx bxs-ghost bx-tada"
     }
+    ,
+    {
+        nombre : "Neat",
+        categoria : "Backgrounds",
+        imagen : "https://res.cloudinary.com/cosmocloudinary/image/upload/freesets/backgrounds/neat",
+        url : "https://neat.firecms.co/",
+        iconcategoria : "bx bxs-ghost bx-tada"
+    },
+    {
+        nombre : "FFFuel",
+        categoria : "Backgrounds",
+        imagen : "https://res.cloudinary.com/cosmocloudinary/image/upload/freesets/backgrounds/neat",
+        url : "https://www.fffuel.co/",
+        iconcategoria : "bx bxs-ghost bx-tada"
+    },
+    {
+        nombre : "SVG Backgrounds",
+        categoria : "Backgrounds",
+        imagen : "https://res.cloudinary.com/cosmocloudinary/image/upload/freesets/backgrounds/svg_backgrounds",
+        url : "https://www.svgbackgrounds.com/",
+        iconcategoria : "bx bxs-ghost bx-tada"
+    },
+    {
+        nombre : "Shape Divider",
+        categoria : "Backgrounds",
+        imagen : "https://res.cloudinary.com/cosmocloudinary/image/upload/freesets/backgrounds/shapedivider",
+        url : "https://www.shapedivider.app/",
+        iconcategoria : "bx bxs-ghost bx-tada"
+    }
+
 ]
