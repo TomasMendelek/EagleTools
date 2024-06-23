74873201fbc21196e512fccfbb83177c33664d9e
tomasmendelek <tomascmendelek@gmail.com>
2024-06-22 21:34:38 -0300

    + Paginas

--- src/data/Pags.js
+++ src/data/Pags.js
@@ -69,7 +69,7 @@ export const Paginas = [
         nombre : "UVcanvas",
         categoria : "Backgrounds",
         imagen : "https://res.cloudinary.com/cosmocloudinary/image/upload/freesets/backgrounds/uvcanvas",
-        url : "https://res.cloudinary.com/cosmocloudinary/image/upload/freesets/backgrounds/uvcanvas",
+        url : "https://uvcanvas.com/",
         iconcategoria : "bx bxs-ghost bx-tada"
     },
     {
@@ -90,7 +90,7 @@ export const Paginas = [
     {
         nombre : "FFFuel",
         categoria : "Backgrounds",
-        imagen : "https://res.cloudinary.com/cosmocloudinary/image/upload/freesets/backgrounds/neat",
+        imagen : "https://res.cloudinary.com/cosmocloudinary/image/upload/freesets/backgrounds/fffuel",
         url : "https://www.fffuel.co/",
         iconcategoria : "bx bxs-ghost bx-tada"
     },
@@ -107,6 +107,117 @@ export const Paginas = [
         imagen : "https://res.cloudinary.com/cosmocloudinary/image/upload/freesets/backgrounds/shapedivider",
         url : "https://www.shapedivider.app/",
         iconcategoria : "bx bxs-ghost bx-tada"
-    }
-
+    },
+    {
+        nombre : "Squoosh",
+        categoria : "Tools",
+        imagen : "https://res.cloudinary.com/cosmocloudinary/image/upload/v1703696273/freesets/tools/tix7qdzgrhabjjyyhqfd.webp",
+        url : "https://squoosh.app/",
+        iconcategoria : "bx bxs-ghost bx-tada"
+    },
+    {
+        nombre : "Shots.so",
+        categoria : "Tools",
+        imagen : "https://res.cloudinary.com/cosmocloudinary/image/upload/v1703007575/freesets/tools/shots.so__mntgnr.webp",
+        url : "https://shots.so/",
+        iconcategoria : "bx bxs-ghost bx-tada"
+    },
+    {
+        nombre : "Excarlidraw",
+        categoria : "Tools",
+        imagen : "https://res.cloudinary.com/cosmocloudinary/image/upload/v1703696414/freesets/tools/x8cznjuhqty30kj7aabe.webp",
+        url : "https://excalidraw.com/",
+        iconcategoria : "bx bxs-ghost bx-tada"
+    },
+    {
+        nombre : "Glyphy.io",
+        categoria : "Tools",
+        imagen : "https://res.cloudinary.com/cosmocloudinary/image/upload/v1704379365/freesets/tools/diets4yvtwotg7yvi8do.webp",
+        url : "https://glyphy.io/es",
+        iconcategoria : "bx bxs-ghost bx-tada"
+    },
+    {
+        nombre : "Open Dooodles",
+        categoria : "illustrations",
+        imagen : "https://res.cloudinary.com/cosmocloudinary/image/upload/freesets/illustrations/opendoodles",
+        url : "https://www.opendoodles.com/",
+        iconcategoria : "bx bxs-ghost bx-tada"
+    },
+    {
+        nombre : "Open Peeps",
+        categoria : "illustrations",
+        imagen : "https://res.cloudinary.com/cosmocloudinary/image/upload/v1705769882/freesets/illustrations/pjkh9luocxnzvbjzciay.webp",
+        url : "https://www.openpeeps.com/",
+        iconcategoria : "bx bxs-ghost bx-tada"
+    },
+    {
+        nombre : "Shapes",
+        categoria : "illustrations",
+        imagen : "https://res.cloudinary.com/cosmocloudinary/image/upload/v1704587990/freesets/illustrations/mezhdpcgrgwri14okctx.webp",
+        url : "https://shapes.framer.website/",
+        iconcategoria : "bx bxs-ghost bx-tada"
+    },
+    {
+        nombre : "3D Khagwal",
+        categoria : "illustrations",
+        imagen : "https://res.cloudinary.com/cosmocloudinary/image/upload/freesets/illustrations/khagwal",
+        url : "https://3d.khagwal.com/",
+        iconcategoria : "bx bxs-ghost bx-tada"
+    },
+    {
+        nombre : "404 kapwing",
+        categoria : "illustrations",
+        imagen : "https://res.cloudinary.com/cosmocloudinary/image/upload/freesets/illustrations/kapwing404",
+        url : "https://www.kapwing.com/404-illustrations",
+        iconcategoria : "bx bxs-ghost bx-tada"
+    },
+    {
+        nombre : "illustration kit",
+        categoria : "illustration",
+        imagen : "https://res.cloudinary.com/cosmocloudinary/image/upload/v1705801178/freesets/illustrations/gbizkz4urhlgioz60okp.webp",
+        url : "https://illustrationkit.com/",
+        iconcategoria : "bx bxs-ghost bx-tada"
+    },
+    {
+        nombre : "Realtime Colors",
+        categoria : "colors",
+        imagen : "https://res.cloudinary.com/cosmocloudinary/image/upload/freesets/colors/realtime-colors",
+        url : "https://www.realtimecolors.com/?colors=050315-fbfbfe-2f27ce-dedcff-433bff&fonts=Inter-Inter",
+        iconcategoria : "bx bxs-ghost bx-tada"
+    },
+    {
+        nombre : "IA Palette Generator",
+        categoria : "colors",
+        imagen : "https://res.cloudinary.com/cosmocloudinary/image/upload/freesets/colors/ai-colors",
+        url : "https://tintmint.net/",
+        iconcategoria : "bx bxs-ghost bx-tada"
+    },
+    {
+        nombre : "Coolors",
+        categoria : "colors",
+        imagen : "https://res.cloudinary.com/cosmocloudinary/image/upload/freesets/colors/coolors",
+        url : "https://coolors.co/",
+        iconcategoria : "bx bxs-ghost bx-tada"
+    },
+    {
+        nombre : "grabient",
+        categoria : "colors",
+        imagen : "https://res.cloudinary.com/cosmocloudinary/image/upload/v1706203980/freesets/colors/g3flh0qgawnbey9te9t2.webp",
+        url : "https://www.grabient.com/",
+        iconcategoria : "bx bxs-ghost bx-tada"
+    },
+    {
+        nombre : "ColorBeta",
+        categoria : "colors",
+        imagen : "https://res.cloudinary.com/cosmocloudinary/image/upload/freesets/colors/colorbeta",
+        url : "https://colorbeta.com/",
+        iconcategoria : "bx bxs-ghost bx-tada"
+    },
+    {
+        nombre : "Pigment",
+        categoria : "colors",
+        imagen : "https://res.cloudinary.com/cosmocloudinary/image/upload/v1706206051/freesets/colors/mxpvc3dhmnidvwu3hnkq.webp",
+        url : "https://pigment.shapefactory.co/",
+        iconcategoria : "bx bxs-ghost bx-tada"
+    },
 ]
