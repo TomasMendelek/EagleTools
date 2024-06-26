8035c7f2a2742a187dc72d3619bdb022131435a4
tomasmendelek <tomascmendelek@gmail.com>
2024-06-26 10:11:36 -0300

    fixed

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,21 +2,19 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+	  extend: {
 		colors: {
-			'backgound': '#010104',
-			'primary': '#c22466',
-			'secundary': '#7d09dc',
-			'tercero': '#4304764c',
-			'accent': '#d78ecc',
-			'text': '#eae9fc',
-			"white": "white",
-			"grey": "grey",
-			"lightgrey": "lightgrey",
-			"black":"black",
-			"blackop": "#000000a4"
-		  },
+		  'backgound': '#090111',
+		  'primary': '#c22466',
+		  'secundary': '#7d09dc',
+		  'tercero': '#4304764c',
+		  'accent': '#d78ecc',
+		  'text': '#eae9fc',
+		  'blackop': '#000000a4'
 		  
+		},
+	  },
 	},
 	plugins: [],
-}
+  }
+  
